090c5117710d64e88d2e51ae201fa1790674193d
Khang <ng.khang2019@gmail.com>
2025-12-02 08:25:53 +0700

    Initial commit

---
+++ src/main/java/com/practice/springjdbcprj1/Springjdbcprj1Application.java
@@ -0,0 +1,13 @@
+package com.practice.springjdbcprj1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springjdbcprj1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springjdbcprj1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/springjdbcprj1/Springjdbcprj1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.springjdbcprj1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springjdbcprj1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
